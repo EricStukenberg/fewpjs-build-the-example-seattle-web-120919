2d3b05f980b149a8dd72f5569731a02bea5b440f
github username <github email address>
2020-02-06 14:46:52 -0800

    Done.

--- main.js
+++ main.js
@@ -3,8 +3,42 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', (even) => {
+  const error_msg = document.getElementById("modal");
+  error_msg.style.display = "none";
+  console.log(error_msg);
+  const like_hearts = document.getElementsByClassName("like-glyph");
+  console.log(like_hearts.length);
+  let len = like_hearts.length;
+  for(let i = 0; i < len; i++ ) {
+    like_hearts[i].addEventListener("click", () => like(like_hearts[i])); 
+  }
+
+}); 
 
 
+
+function like(like_heart) {
+  console.log(like_heart.innerHTML);
+
+    mimicServerCall().then(() => {
+      if(like_heart.innerHTML === EMPTY_HEART) {
+        like_heart.innerHTML = FULL_HEART;
+        like_heart.style.color = "red";
+      } else {
+        like_heart.innerHTML = EMPTY_HEART;
+        like_heart.style.color = "";
+      }
+    }).catch((error) => {
+      const error_msg = document.getElementById("modal");
+      error_msg.style.display = "block";
+      console.log(error);
+      setTimeout(() => {error_msg.style.display = "none";}, 1000)
+      console.log("AFTER SLEEP");
+    });
+
+
+}
 
 
 //------------------------------------------------------------------------------
@@ -23,3 +57,5 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
     }, 300);
   });
 }
+
+
